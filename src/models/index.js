612bc415930049647822c1eeaecc66fce24dc3a3
Mayank Singh <68012074+fms12@users.noreply.github.com>
2023-12-31 00:02:44 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -5,10 +5,9 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.json')[env];
-const env = process.env.NODE_ENV || "development";
-const config = env === "production" ? production : env === "test" ? test : development;
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+
 
 const db = {};
 
