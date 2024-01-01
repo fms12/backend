a8142709872746109d2cc56fcd22e1b74c8c03ea
fms12 <mayank2001.iimt@gmail.com>
2024-01-01 23:21:44 +0530

    database connection

---
+++ src/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "password",
+    database: process.env.DB_NAME || "todo_db_dev",
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "database_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "todo_db_dev",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+  },
+};
