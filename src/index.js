b1dfbd860f433d3a10bb6fc743412c2a119caf08
fms12 <mayank2001.iimt@gmail.com>
2024-01-02 00:27:59 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const {PORT} = require("./config/serverConfig")
-
+const cors = require("cors");
 const ApiRoutes = require("./routes/index");
 
 const  setupAndStartService = ()=>{
 
     const app = express();
+    app.use(cors());
     app.use(express.json()); //For JSON requests
     app.use(express.urlencoded({ extended: true }));
     app.use('/api', ApiRoutes)
